fb50c15446c89cd2e3737a1f947a3e77b9a79d41
Totiruzi <totiruzi@yahoo.com>
2022-11-24 23:15:22 +0300

    Refactor UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Different Services provided to an App with the same alias class';
+  title = 'Using Value Providers in an Angular App';
   constructor(
     private authservice: AuthService,
     private router: Router
